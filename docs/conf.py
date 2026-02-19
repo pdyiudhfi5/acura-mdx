9940db38d291c501d9e4f548d364d9cda28462d5
pdyiudhfi5 <pdyiudhfi5@outlook.com>
2026-02-19 16:04:58 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,15 @@
-project = 'acura mdx'
-author = 'acura-mdx'
+project = "acura GPS"
+author = 'Your Name'
 release = '1.0'
 
-# Extensions
-extensions = [
-    'sphinx_sitemap',
-]
-
-# Paths
+extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-# Theme
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# Custom JS & Favicon
-html_js_files = ['chatbot.js']  # chatbot widget
-html_favicon = '_static/favicon.png'
-
-# Bing search verification
-html_context = {
-    'bing_verification_code': '89E119164B044B0B4EE80A50AB60418E'
-}
-
-# Base URL for sitemap
-html_baseurl = 'https://acura-mdx.readthedocs.io/en/latest/'
+html_js_files = [
+    'chat.js',
+]
+html_favicon = '_static/favion.png'
